9bec0ca706afc6c514b0f9ae18b5b8a61bc3a115
poojaramnaney <poojaramnaney@gmail.com>
2023-06-14 22:52:30 +0530

    Deploying Vite React website to GitHub Pages

--- src/components/Header/Navbar.jsx
+++ src/components/Header/Navbar.jsx
@@ -63,12 +63,12 @@ const Navbar = () => {
               
               <div className="py-1">
                 <a className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900"
-                  onClick={() => handleClick("memory")}>
+                  onClick={() => handleClick("trivia")}>
                     
                   Memory Game
                 </a>
                 <a className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900"
-                  onClick={() => handleClick("trivia")}>
+                  onClick={() => handleClick("memory")}>
                   Trivia
                 </a>
                 
